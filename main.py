a67493363a9ef5b0ebb871579d21a7ee2c536544
dmitr <2madeira@mail.ru>
2021-05-01 06:52:16 +0300

    fix: added interaction with the database containing questions and answers about the game (before, questions and answers were in the dictionary). reworked the logic of winning and displaying letters belonging to the hidden word

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import random
 import os.path
+import sqlite3 as sq
+import re
+
+
 
 
 def print_man(man, answered_byk):
@@ -21,11 +25,11 @@ def print_man(man, answered_byk):
 
 
 def hod_korolevy(
-    score, questions, question, man, delete_chast, answered_byk, player_name
+    score, qanswer, question, man, delete_chast, answered_byk, player_name
 ):
     print_man(man, answered_byk)
     while True:
-        if score == len(questions[question]):
+        if len(answered_byk) == len(qanswer) and (not '_' in answered_byk):
             print("Вы выиграли!")
             break
         print("Угадаете букву? Или хотите сохраниться? Тогда введите 'save'")
@@ -49,16 +53,23 @@ def hod_korolevy(
             for i in answered_byk:
                 f.write(str(i))
             f.write("\n")
-            f.write(player_name)
+            f.write(player_name + "\n")
+            f.write(qanswer)
             f.close()
+            print("Сохранение успешно завершено!")
         else:
+
             if (
-                buk in questions[question] or buk in questions[question].upper()
+                buk in qanswer or buk in qanswer.upper() or buk in qanswer.lower()
             ) and buk not in answered_byk:
                 score += 1
                 print(f"Absolutely right! Колво ваших очков теперь: {score}")
                 buk = buk.lower()
-                answered_byk[questions[question].index(buk)] = buk
+                qanswer = qanswer.lower()
+                l = [i for i in range(len(qanswer)) if qanswer[i] == buk]
+                for i in l:
+                    answered_byk[i] = buk
+
             else:
                 man[delete_chast] = " "
                 delete_chast += 1
@@ -69,7 +80,17 @@ def hod_korolevy(
 
 
 while True:
-    questions = {"who am i": "dima", "who are you": "pidor"}
+    with sq.connect("Saper.db") as con:
+        cur = con.cursor()
+        idd = random.randrange(1, 5)
+        cur.execute(f"SELECT question FROM questionshmm WHERE id = {idd}")
+        result = cur.fetchall()
+        result = str(result)
+        question = re.sub("[\[\](',)]", "", result)
+        cur.execute(f"SELECT tanswer FROM questionshmm WHERE id = {idd}")
+        result = cur.fetchall()
+        result = str(result)
+        qanswer = re.sub("[\[\](',)]", "", result)
     print(
         "Do you want to start the new game? y/n However, if you want to load the last save, enter 'load'"
     )
@@ -77,38 +98,38 @@ while True:
     if answer == "y":
         answered_byk = []
 
-        questions_mas = ["who am i", "who are you"]
+        #questions_mas = ["who am i", "who are you"]
         man = ["/", "\\", "|", "\\", "|", "/", "O"]
         print("Hello! This is a Hangman!!! What is your name???")
         player_name = input()
         score = 0
         print("Good, Let's start game!")
         delete_chast = 0
-        question = questions_mas[random.randrange(0, len(questions_mas))]
-        for i in range(0, len(questions[question])):
+        for i in range(0, len(qanswer)):
             answered_byk.append("_")
 
         print(f"Question: {question}")
 
         hod_korolevy(
-            score, questions, question, man, delete_chast, answered_byk, player_name
+            score, qanswer, question, man, delete_chast, answered_byk, player_name
         )
     elif answer == "load":
         if os.path.isfile("text.txt"):
             f = open("text.txt", "r")
             text = f.read()
-            text = text.split("\n")
+            text = text.split("\n")   #сделать перезапись qanswer
             score = int(text[0])
             question = text[1]
             man = list(text[2])
             delete_chast = int(text[3])
             answered_byk = list(text[4])
             player_name = text[5]
+            qanswer = text[6]
             print("Загрузка последнего сохранения успешно завершилась!")
             print(f"Игрок: {player_name}. Количество очков: {score} ")
             print(f"Вопрос: {question}")
             hod_korolevy(
-                score, questions, question, man, delete_chast, answered_byk, player_name
+                score, qanswer, question, man, delete_chast, answered_byk, player_name
             )
         else:
             print("Sorry, сохранение куда то подевалось, начните новую игру...")
